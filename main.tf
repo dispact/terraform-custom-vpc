bfe59c7393b5b4d62a7e2d60d0008989434457e9
Sai kiran Bavandla <155256704+Sai-kiran-bavandla@users.noreply.github.com>
2024-03-13 15:02:25 +0000

    3

--- main.tf
+++ main.tf
@@ -1,22 +1,13 @@
-// Here is where we are defining
-// our Terraform settings
-terraform {
-  required_providers {
-    // The only required provider we need
-    // is aws, and we want version 4.0.0
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.39.1"
-    }
-  }
+// module for terraform versions
+module  "a_versions" {
+  source = "./module/a_versions"
 
-  // This is the required version of Terraform
-  required_version = "~> 1.7.4"
+  db_username = "admin"
+  db_password = "admin"
+  my_ip = "10.0.0.239"
 }
 
-// Here we are configuring our aws provider. 
-// We are setting the region to the region of 
-// our variable "aws_region"
+
 provider "aws" {
   region = var.aws_region
 }
